8d9d038226ee4be0c8c9df30e1b2ea75408145bf
Julien Dutheil <jy.dutheil@gmail.com>
2010-06-03 14:53:00 +0000

    New method in bppPhySamp!

--- bppSuite/bppPhyloSampler.cpp
+++ bppSuite/bppPhyloSampler.cpp
@@ -78,11 +78,35 @@ void help()
   (*ApplicationTools::message << "__________________________________________________________________________").endLine();
 }
 
+class Index {
+  public:
+    double distance;
+    unsigned int i1, i2;
+
+  public:
+    Index(double dist, unsigned int i, unsigned int j) : distance(dist), i1(i), i2(j) {}
+
+  public:
+    bool operator==(const Index& index) const { return distance == index.distance; }
+    bool operator<(const Index& index) const { return distance < index.distance; }
+};
+
+class Test {
+  private:
+    unsigned int pos_;
+
+  public:
+    Test(unsigned int pos) : pos_(pos) {}
+    
+  public:
+    bool operator()(const Index& index) { return index.i1 == pos_ || index.i2 == pos_; }
+};
+
 int main(int args, char ** argv)
 {
   cout << "******************************************************************" << endl;
-  cout << "*           Bio++ Phylogenetic Sampler, version 0.1              *" << endl;
-  cout << "* Author: J. Dutheil                        Last Modif. 08/08/09 *" << endl;
+  cout << "*           Bio++ Phylogenetic Sampler, version 0.2              *" << endl;
+  cout << "* Author: J. Dutheil                        Last Modif. 03/06/10 *" << endl;
   cout << "******************************************************************" << endl;
   cout << endl;
   
@@ -104,7 +128,7 @@ int main(int args, char ** argv)
   string inputMethod = ApplicationTools::getStringParameter("input.method", bppphysamp.getParams(), "tree");
   ApplicationTools::displayResult("Input method", inputMethod);
 
-  DistanceMatrix* dist = NULL;
+  DistanceMatrix* dist = 0;
   if(inputMethod == "tree")
   {
     Tree* tree = PhylogeneticsApplicationTools::getTree(bppphysamp.getParams());
@@ -118,11 +142,11 @@ int main(int args, char ** argv)
   }
   else throw Exception("Unknown input method: " + inputMethod);
 
+  string deleteMeth = ApplicationTools::getStringParameter("deletion_method", bppphysamp.getParams(), "threshold");
+  ApplicationTools::displayResult("Deletion method", deleteMeth);
+
   string critMeth = ApplicationTools::getStringParameter("choice_criterion", bppphysamp.getParams(), "length");
   ApplicationTools::displayResult("Sequence choice criterion", critMeth);
-
-  double threshold = ApplicationTools::getDoubleParameter("threshold", bppphysamp.getParams(), 0.01);
-  ApplicationTools::displayResult("Distance threshold", threshold);
 
   //Compute lengths:
   vector<string> seqNames;
@@ -138,37 +162,78 @@ int main(int args, char ** argv)
     seqNames.push_back(name);
   }
 
-  unsigned int rm = 0;
-  for(unsigned int i = 0; i < dist->size()-1; i++)
-  {
-    for(unsigned int j = i+1; j < dist->size(); j++)
-    {
-      //if((*dist)(i,j) < -log(0.)) cout << (*dist)(i, j) << endl;
-      if((*dist)(i, j) <= threshold)
-      {
-        //We need to chose between the two sequences:
-        if(critMeth == "length" || critMeth == "length.complete")
-        {
-          if(seqLen[i] > seqLen[j]) rm = j; else rm = i;
-        }
-        else if(critMeth == "random")
-          if(RandomTools::flipCoin()) rm = j; else rm = i;
-        else throw Exception("Unknown cirterion: " + critMeth);
+  //Sort matrix entries:
+  vector<Index> distances;
+  for (unsigned int i = 0; i < dist->size()-1; i++)
+    for (unsigned int j = i+1; j < dist->size(); j++)
+      distances.push_back(Index((*dist)(i, j), i , j));
+  sort(distances.begin(), distances.end());
 
-        //Remove sequence in list:
-        unsigned int pos = VectorTools::which(seqNames, dist->getName(rm));
-        ApplicationTools::displayResult("Remove sequence", seqNames[pos]);
-        seqNames.erase(seqNames.begin() + pos); 
-        
-        //Ignore all distances from this sequence:
-        for(unsigned int k = 0; k < dist->size(); k++)
-        {
-          (*dist)(rm, k) = (*dist)(k, rm) = -log(0.); 
-        }
+  if (deleteMeth == "threshold")
+  {
+    double threshold = ApplicationTools::getDoubleParameter("threshold", bppphysamp.getParams(), 0.01);
+    ApplicationTools::displayResult("Distance threshold", threshold);
+
+    unsigned int rm = 0;
+    while (distances[0].distance <= threshold)
+    {
+      //We need to chose between the two sequences:
+      if (critMeth == "length" || critMeth == "length.complete")
+      {
+        if (seqLen[distances[0].i1] > seqLen[distances[0].i2]) rm = distances[0].i2;
+        else rm = distances[0].i1;
       }
+      else if (critMeth == "random")
+      {
+        if (RandomTools::flipCoin()) rm = distances[0].i2;
+        else rm = distances[0].i1;
+      }
+      else throw Exception("Unknown criterion: " + critMeth);
+
+      //Remove sequence in list:
+      unsigned int pos = VectorTools::which(seqNames, dist->getName(rm));
+      ApplicationTools::displayResult("Remove sequence", seqNames[pos]);
+      seqNames.erase(seqNames.begin() + pos); 
+        
+      //Ignore all distances from this sequence:
+      remove_if(distances.begin(), distances.end(), Test(rm));
+      if (distances.size() == 0)
+        throw Exception("Error, all sequences have been removed with this criterion!");
     }
+    ApplicationTools::displayResult("Number of sequences kept:", seqNames.size());
   }
-  ApplicationTools::displayResult("Number of sequences kept:", seqNames.size());
+  else if (deleteMeth == "sample")
+  {
+    unsigned int sampleSize = ApplicationTools::getParameter<unsigned int>("sample_size", bppphysamp.getParams(), 10);
+    ApplicationTools::displayResult("Sample size", sampleSize);
+    
+    unsigned int rm = 0;
+    while (seqNames.size() > sampleSize)
+    {
+      //We need to chose between the two sequences:
+      if (critMeth == "length" || critMeth == "length.complete")
+      {
+        if (seqLen[distances[0].i1] > seqLen[distances[0].i2]) rm = distances[0].i2;
+        else rm = distances[0].i1;
+      }
+      else if (critMeth == "random")
+      {
+        if (RandomTools::flipCoin()) rm = distances[0].i2;
+        else rm = distances[0].i1;
+      }
+      else throw Exception("Unknown criterion: " + critMeth);
+
+      //Remove sequence in list:
+      unsigned int pos = VectorTools::which(seqNames, dist->getName(rm));
+      ApplicationTools::displayResult("Remove sequence", seqNames[pos]);
+      seqNames.erase(seqNames.begin() + pos); 
+        
+      //Ignore all distances from this sequence:
+      remove_if(distances.begin(), distances.end(), Test(rm));
+    }
+    ApplicationTools::displayResult("Minimal distance in final data set:", distances[0].distance);
+  }
+  else throw Exception("Unknown deletion method: " + deleteMeth + ".");
 
   //Write sequences to file:
   AlignedSequenceContainer asc(alphabet);
