39c71e94c9fcceb1e4b42edbf98d5f909f45bb00
Julien Dutheil <jy.dutheil@gmail.com>
2013-03-15 11:55:28 +0000

    Fixed typo in file name.

--- buildBin.sh
+++ buildBin.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-arch=i686
+arch=x86_64 #i686
 version=0.8.0-1
 
 strip bppSuite/bppdist
@@ -14,5 +14,5 @@ strip bppSuite/bppancestor
 strip bppSuite/bpptreedraw
 strip bppSuite/bppalnscore
 strip bppSuite/bppmixedlikelihoods
-tar cvzf bppSuite-${arch}-bin-static-${version}.tar.gz bppSuite/bppdist bppSuite/bpppars bppSuite/bppml bppSuite/bppseqgen bppSuite/bppconsense bppSuite/bppseqman bppSuite/bppphysamp bppSuite/bppreroot bppSuite/bppancestor bppSuite/bpptreedraw bppSuite/bppalnscore bppSuite/bppmixedlikelihoods
+tar cvzf bppsuite-${arch}-bin-static-${version}.tar.gz bppSuite/bppdist bppSuite/bpppars bppSuite/bppml bppSuite/bppseqgen bppSuite/bppconsense bppSuite/bppseqman bppSuite/bppphysamp bppSuite/bppreroot bppSuite/bppancestor bppSuite/bpptreedraw bppSuite/bppalnscore bppSuite/bppmixedlikelihoods
 
--- buildExample.sh
+++ buildExample.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
-zip -r bppSuite-examples-0.8.0.zip Examples -x \*.svn\*
+zip -r bppsuite-examples-0.8.0.zip Examples -x \*.svn\*
 
